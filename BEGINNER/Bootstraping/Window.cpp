6099026d6568c2a4058047ec12e661e6c63324db
rasteiro11 <titico0306@gmail.com>
2022-07-28 18:21:54 -0300

    user input handling

--- BEGINNER/Bootstraping/Window.cpp
+++ BEGINNER/Bootstraping/Window.cpp
@@ -1,13 +1,22 @@
 #include "Window.h"
+#include <GLFW/glfw3.h>
+#include <cstdio>
 
 Window::Window() {
   width = 800;
   height = 600;
+
+  for (size_t i = 0; i < 1024; i++) {
+    keys[i] = 0;
+  }
 }
 
 Window::Window(GLint windowWidth, GLint windowHeight) {
   width = windowWidth;
   height = windowHeight;
+  for (size_t i = 0; i < 1024; i++) {
+    keys[i] = 0;
+  }
 }
 
 int Window::Initialize() {
@@ -33,6 +42,9 @@ int Window::Initialize() {
 
   glfwMakeContextCurrent(mainWindow);
 
+  createCallbacks();
+  glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
   glewExperimental = GL_TRUE;
 
   if (glewInit() != GLEW_OK) {
@@ -45,7 +57,57 @@ int Window::Initialize() {
   glEnable(GL_DEPTH_TEST);
 
   glViewport(0, 0, bufferWidth, bufferHeight);
+
+  glfwSetWindowUserPointer(mainWindow, this);
+
   return 0;
+}
+
+void Window::createCallbacks() {
+  glfwSetKeyCallback(mainWindow, handleKeys);
+  glfwSetCursorPosCallback(mainWindow, handleMouse);
+}
+
+GLfloat Window::getXChange() {
+  GLfloat theChange = xChange;
+  xChange = 0.0f;
+  return theChange;
+}
+GLfloat Window::getYChange() {
+  GLfloat theChange = yChange;
+  yChange = 0.0f;
+  return theChange;
+}
+
+void Window::handleKeys(GLFWwindow *window, int key, int code, int action,
+                        int mode) {
+  Window *theWindow = static_cast<Window *>(glfwGetWindowUserPointer(window));
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GL_TRUE);
+  }
+  if (key >= 0 && key < 1024) {
+    if (action == GLFW_PRESS) {
+      theWindow->keys[key] = true;
+      printf("Pressed: %d\n", key);
+    } else if (action == GLFW_RELEASE) {
+      theWindow->keys[key] = false;
+      printf("Released: %d\n", key);
+    }
+  }
+}
+
+void Window::handleMouse(GLFWwindow *window, double xPos, double yPos) {
+  Window *theWindow = static_cast<Window *>(glfwGetWindowUserPointer(window));
+  if (theWindow->mouseFirstMoved) {
+    theWindow->lastX = xPos;
+    theWindow->lastY = yPos;
+    theWindow->mouseFirstMoved = false;
+  }
+  theWindow->xChange = xPos - theWindow->lastX;
+  theWindow->yChange = theWindow->lastY - yPos;
+
+  theWindow->lastX = xPos;
+  theWindow->lastY = yPos;
 }
 
 Window::~Window() {
